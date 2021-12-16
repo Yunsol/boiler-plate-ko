abea11f64cad53234d5358524e3a9b913a1fcb42
Yunsol <c21154035@gmail.com>
2021-12-16 20:01:07 +0900

    급히 커밋

--- models/User.js
+++ models/User.js
@@ -1,4 +1,8 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const jwt = require('jsonwebtoken');
+
 
 const userSchema = mongoose.Schema({
     name: {
@@ -12,7 +16,7 @@ const userSchema = mongoose.Schema({
     },
     password: {
         type: String,
-        minlength: 50
+        minlength: 5
     },
     role: {
         type: Number,
@@ -26,6 +30,58 @@ const userSchema = mongoose.Schema({
         type: Number
     }
 })
+
+userSchema.pre('save', function(next){
+    var user = this;
+
+    if (user.isModified('password')) {
+        //배밀번호를 암호화 시킴
+        bcrypt.genSalt(saltRounds, function(err, salt) {
+            // Store hash in your password DB.
+            if(err) return next(err)
+            bcrypt.hash(user.password, salt, function(err, hash) {
+                if(err) return next(err)
+                user.password = hash
+                next()
+            });
+        });
+    } else {
+        next()
+    }
+})
+
+userSchema.methods.comparePassword = function(plainPassword, cb) {
+    // plainPassword : 1234567, 암호화된 비밀번호: $2b$10$J65Zg3gutsV3uwVu1i6BFeZiGGPRbTq/zMu6R8OANJVMbApbKX0FC
+    bcrypt.compare(plainPassword, this.password, function(err, isMatch) {
+        if(err) return cb(err)
+        cb(null, isMatch)
+    })
+}
+
+userSchema.methods.generateToken = function(cb) {
+    var user = this;
+    //jsonwebtoken을 이용해서 token을 생성하기
+    var token = jwt.sign(user._id.toHexString(), 'secretToken')
+    user.token = token
+    user.save(function(err, user) {
+        if(err) return cb(err)
+        cb(null, user)
+    })
+
+}
+
+userSchema.statics.findByToken = function(token, cb) {
+    var user = this;
+
+    // 토큰을 decode한다.
+    jwt.verify(token, 'secretToken', function(err, decoded){
+        // 유저아이디를 이용해서 유저를 찾은 다음에 클라이언트에서 가져온 토큰과 데이터베이스에 보관된 토큰이 일치하는지 확인
+        user.findOne({"_id": decoded, "token": token}, function(err, user){
+            if(err) return cb(err);
+            cb(null, user)
+        })
+    })
+}
 
 const User = mongoose.model('User', userSchema)
 
